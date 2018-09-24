3d476da0799ca871fc48be813333ccdde9fdf39b
Fabian Mildner <fabian-mildner@t-online.de>
2018-09-24 20:11:43 +0200

    Enable timer states to be saved and compared

--- src/de/openhpi/capstone1/game/builder/Game.java
+++ src/de/openhpi/capstone1/game/builder/Game.java
@@ -1,11 +1,13 @@
 package de.openhpi.capstone1.game.builder;
 
+import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
 import de.openhpi.capstone1.game.controller.BallController;
 import de.openhpi.capstone1.game.controller.PaddleControllerStrategyLeft;
 import de.openhpi.capstone1.game.controller.PaddleControllerStrategyRight;
 import de.openhpi.capstone1.game.controller.TimerController;
+import de.openhpi.capstone1.game.exceptions.BallDepotDepletedException;
 import de.openhpi.capstone1.game.model.Ball;
 import de.openhpi.capstone1.game.model.BallDepot;
 import de.openhpi.capstone1.game.model.GameField;
@@ -25,11 +27,13 @@ public class Game extends InteractiveComponent {
 	private boolean gameActive;
 	
 	private PApplet display;
+	
 	private GameField gameField;
 	private Paddle paddle;
 	private BallDepot ballDepot;
-	private Ball ball;
+	private Ball activeBall;
 	private Timer timer;
+	private ArrayList<int[]> timerResults = new ArrayList<int[]>();
 	
 	private PaddleControllerStrategyRight paddleControlStratRight;
 	private PaddleControllerStrategyLeft paddleControlStratLeft;
@@ -50,13 +54,17 @@ public class Game extends InteractiveComponent {
 			timerController.handleEvent();
 			ballController.handleEvent();			
 			
-//			if (ball.getY() > gameField.getBallDeathLine()) {
-//				// TODO  Ball has died. Perform action!
-//				ballController = null;
-//				ballView = null;
-//				ball = null;
-//				
-//		}
+			if (activeBall.getY() > gameField.getBallDeathLine()) {
+				// Kill the activeBall
+				ballController = null;
+				views[views.length-1] = new BallView(display, new Ball(-200, -200, 10, 0, 0)); // activeBall view is always last element of views array. To kill current one, insert dummy view instead
+				activeBall = null;
+				
+				// Save timer result
+				timerResults.add(timer.exportState());
+				
+				gameActive = false;
+			}
 		
 		}
 		
@@ -67,8 +75,27 @@ public class Game extends InteractiveComponent {
 	public void handleEvent() {
 		switch (display.key) {
 			case ' ':
-				// TODO: handle ball reset or balldepot logic if ever implemented
-				break;
+				if (!gameActive) {
+					try {
+						float INIT_BALL_VELOCITY_X = ThreadLocalRandom.current().nextInt(1, 3+1); // randomize activeBall's velocities
+						float INIT_BALL_VELOCITY_Y = ThreadLocalRandom.current().nextInt(1, 3+1);
+						
+						activeBall = ballDepot.getNextBall();
+						activeBall.velocityX = INIT_BALL_VELOCITY_X;
+						activeBall.velocityY = INIT_BALL_VELOCITY_Y;
+						activeBall.setX(display.width/2);
+						activeBall.setY(display.height/2);
+						
+						views[views.length-1] = new BallView(display, activeBall); // activeBall view is always last element of views array
+						ballController = new BallController(activeBall, paddle, gameField, 10, display.frameRate);
+						
+						timer.resetTimer();
+						
+						gameActive = true;
+					} catch (BallDepotDepletedException e) {
+						// do nothing. At this point, the game over screen is shown and covers everything else
+					}
+				}
 			case PConstants.CODED: // key is coded a special way; need to check keyCode instead
 				switch (display.keyCode) {
 					case PConstants.LEFT:
@@ -86,7 +113,7 @@ public class Game extends InteractiveComponent {
 		float GAME_FIELD_X = display.width/2;
 		float GAME_FIELD_WIDTH = display.width*8/10;
 		float GAME_FIELD_DISTANCE_TOP = display.height*2/10;
-		float GAME_FIELD_BALL_DEATH_LINE = display.height + 20; // ball is considered dead once it's 20 pixels below window 
+		float GAME_FIELD_BALL_DEATH_LINE = display.height + 20; // activeBall is considered dead once it's 20 pixels below window 
 		
 		float PADDLE_Y_POS = display.height - 30; // 30 pixels above the bottom of the pit
 		float PADDLE_WIDTH = GAME_FIELD_WIDTH/8;
@@ -95,8 +122,6 @@ public class Game extends InteractiveComponent {
 		float PADDLE_BOUND_RIGHT = GAME_FIELD_X + GAME_FIELD_WIDTH/2 - PADDLE_WIDTH/2;  // maximum x value which the paddle may have
 		
 		float BALL_RADIUS = 8;
-		float INIT_BALL_VELOCITY_X = ThreadLocalRandom.current().nextInt(1, 3+1); // randomize ball's velocities
-		float INIT_BALL_VELOCITY_Y = ThreadLocalRandom.current().nextInt(1, 3+1);
 		
 		float TIMER_X = 10;
 		float TIMER_Y = 35;
@@ -109,7 +134,6 @@ public class Game extends InteractiveComponent {
 		
 		this.gameField = new GameField(GAME_FIELD_X, GAME_FIELD_WIDTH, GAME_FIELD_DISTANCE_TOP, GAME_FIELD_BALL_DEATH_LINE);
 		this.paddle = new Paddle(display.width/2, PADDLE_Y_POS, PADDLE_WIDTH, PADDLE_HEIGHT, PADDLE_BOUND_LEFT, PADDLE_BOUND_RIGHT);
-		this.ball = new Ball(display.width/2, display.height/2, BALL_RADIUS, INIT_BALL_VELOCITY_X, INIT_BALL_VELOCITY_Y);
 		this.ballDepot = new BallDepot(BALL_DEPOT_X, BALL_DEPOT_Y, BALL_DEPOT_WIDTH, BALL_DEPOT_HEIGHT, new Ball[] {new Ball(BALL_RADIUS), new Ball(BALL_RADIUS), new Ball(BALL_RADIUS)});
 		this.timer = new Timer(TIMER_X, TIMER_Y, TIMER_SIZE);
 	}
@@ -119,26 +143,24 @@ public class Game extends InteractiveComponent {
 		views = new AbstractView[] {
 				new GameFieldView(display, gameField),
 				new PaddleView(display, paddle),
-				new BallView(display, ball),
 				new TimerView(display, timer),
-				new BallDepotView(display, ballDepot)
+				new BallDepotView(display, ballDepot),
+				new BallView(display, new Ball(-200, -200, 10, 0, 0)) // placeholder for BallView. BallView is always last element of this array. To circumvent class type problems (i.e. Abstact view or a descendent is required here), just create a dummy view here
 		};
 	}
 	
 	@Override
 	public void buildControllers() {
-		gameActive = true;
-		
 		float PADDLE_STEP_WIDTH = paddle.getWidth()/2;
-		
-		int SECONDS_TILL_BALL_SPEED_UP = 10;
 		
 		paddleControlStratLeft = new PaddleControllerStrategyLeft(paddle, PADDLE_STEP_WIDTH);
 		paddleControlStratRight = new PaddleControllerStrategyRight(paddle, PADDLE_STEP_WIDTH);
 		
-		ballController = new BallController(ball, paddle, gameField, SECONDS_TILL_BALL_SPEED_UP, display.frameRate);
-		
 		timerController = new TimerController(timer, display.frameRate);
+	}
+	
+	private void showGameOverScreen() {
+		
 	}
 
 }
--- src/de/openhpi/capstone1/game/model/Timer.java
+++ src/de/openhpi/capstone1/game/model/Timer.java
@@ -1,5 +1,7 @@
 package de.openhpi.capstone1.game.model;
 
+import java.util.ArrayList;
+
 public class Timer extends PositionableElement {
 	
 	public int tenthSecondsCount;
@@ -18,6 +20,34 @@ public class Timer extends PositionableElement {
 			secondsCount++;
 			tenthSecondsCount = 0;
 		}
+	}
+	
+	public int[] exportState() {
+		return new int[] {secondsCount, tenthSecondsCount};
+	}
+	
+	public void resetTimer() {
+		tenthSecondsCount = 0;
+		secondsCount = 0;
+	}
+	
+	public static int[] findGreatestTime(ArrayList<int[]> timerResults) {
+		
+		int max_full_sec = 0;
+		int max_tenth_sec = 0;
+		for (int[] result : timerResults) {
+			int this_full_sec = result[0];
+			int this_tenth_sec = result[1];
+			
+			if (this_full_sec > max_full_sec) {
+				max_full_sec = this_full_sec;
+				max_tenth_sec = this_tenth_sec;
+			} else if (this_full_sec == max_full_sec && this_tenth_sec > max_tenth_sec) {
+				max_tenth_sec = this_tenth_sec;
+			}
+			
+		}
+		return new int[] {max_full_sec, max_tenth_sec};
 	}
 	
 	
